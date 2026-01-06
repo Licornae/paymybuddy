029efe370e01bb175085a3e8b76399640dd234b2
Caroline <bergere.caroline@outlook.fr>
2026-01-06 16:48:41 +0100

    test: UserServiceTest prevent duplicate users by email and username

---
+++ src/test/java/com/openclassrooms/paymybuddy/serviceTest/UserServiceTest.java
@@ -0,0 +1,70 @@
+package com.openclassrooms.paymybuddy.serviceTest;
+
+import com.openclassrooms.paymybuddy.model.AppUser;
+import com.openclassrooms.paymybuddy.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    public void shouldThrowExceptionWhenEmailAlreadyExists() {
+
+        AppUser existingUser = new AppUser();
+        existingUser.setEmail("test@mail.com");
+
+        when(userRepository.findByEmail("test@mail.com"))
+                .thenReturn(existingUser);
+
+        AppUser newUser = new AppUser();
+        newUser.setUsername("newuser");
+        newUser.setEmail("test@mail.com");
+        newUser.setPassword("password");
+        newUser.setRole("USER");
+
+        Throwable thrown = catchThrowable(() -> userService.saveUser(newUser));
+
+        assertThat(thrown)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Email already exists");
+
+        verify(userRepository, never()).save(any()); // vérifie que userRepository.save() n’est jamais exécuté avec n’importe quel argument
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenUsernameAlreadyExists() {
+
+        AppUser existingUser = new AppUser();
+        existingUser.setUsername("existingUser");
+
+        when(userRepository.findByUsername("existingUser"))
+                .thenReturn(existingUser);
+
+        AppUser newUser = new AppUser();
+        newUser.setUsername("existingUser");
+        newUser.setEmail("new@mail.com");
+        newUser.setPassword("password");
+        newUser.setRole("USER");
+
+        Throwable thrown = catchThrowable(() -> userService.saveUser(newUser));
+
+        assertThat(thrown)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Username already exists");
+
+        verify(userRepository, never()).save(any());
+    }
+
+}
