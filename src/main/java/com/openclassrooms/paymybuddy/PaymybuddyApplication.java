6c0bbe059e15b27c97a63daafdf0c2d069586917
Caroline <bergere.caroline@outlook.fr>
2026-01-06 15:24:47 +0100

    First Commit

---
+++ src/main/java/com/openclassrooms/paymybuddy/PaymybuddyApplication.java
@@ -0,0 +1,19 @@
+package com.openclassrooms.paymybuddy;
+
+import jakarta.transaction.Transactional;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymybuddyApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymybuddyApplication.class, args);
+	}
+
+	@Override
+	@Transactional
+	public void run(String... args) throws Exception {
+	}
+}
---
+++ src/test/java/com/openclassrooms/paymybuddy/PaymybuddyApplicationTests.java
@@ -0,0 +1,9 @@
+package com.openclassrooms.paymybuddy;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PaymybuddyApplicationTests {
+}
