e00b43fbe10c6c1a69ade3cf4f8f4cab42831562
Caroline <bergere.caroline@outlook.fr>
2026-01-07 12:19:42 +0100

    test: ConnectionRepositoryTest add find connection

--- src/test/java/com/openclassrooms/paymybuddy/repositoryTest/ConnectionRepositoryTest.java
+++ src/test/java/com/openclassrooms/paymybuddy/repositoryTest/ConnectionRepositoryTest.java
@@ -1,5 +1,8 @@
 package com.openclassrooms.paymybuddy.repositoryTest;
 
+import com.openclassrooms.paymybuddy.model.Connection;
+import com.openclassrooms.paymybuddy.model.ConnectionId;
+import com.openclassrooms.paymybuddy.repository.ConnectionRepository;
 import com.openclassrooms.paymybuddy.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,5 +46,37 @@ public class ConnectionRepositoryTest {
         Connection saved = connectionRepository.save(connection);
 
         assertThat(connectionRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    public void shouldFindConnectionByCompositeId() {
+
+        AppUser user = new AppUser();
+        user.setUsername("user");
+        user.setEmail("user@mail.com");
+        user.setPassword("password");
+        user.setRole("USER");
+
+        AppUser friend = new AppUser();
+        friend.setUsername("friend");
+        friend.setEmail("friend@mail.com");
+        friend.setPassword("password");
+        friend.setRole("USER");
+
+        userRepository.save(user);
+        userRepository.save(friend);
+
+        Connection connection = new Connection();
+        connection.setUser(user);
+        connection.setFriend(friend);
+
+        connectionRepository.save(connection);
+
+        ConnectionId id = new ConnectionId(
+                user.getIdUser(),
+                friend.getIdUser()
+        );
+
+        assertThat(connectionRepository.findById(id)).isPresent();
     }
 }
