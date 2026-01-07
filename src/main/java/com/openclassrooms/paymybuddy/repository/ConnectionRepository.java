72eb4ab6a48641743a0476ed36e4bb1f651afb01
Caroline <bergere.caroline@outlook.fr>
2026-01-07 11:51:56 +0100

    docs: add javadoc to Connection and repository

--- src/main/java/com/openclassrooms/paymybuddy/repository/ConnectionRepository.java
+++ src/main/java/com/openclassrooms/paymybuddy/repository/ConnectionRepository.java
@@ -4,5 +4,14 @@ import com.openclassrooms.paymybuddy.model.Connection;
 import com.openclassrooms.paymybuddy.model.ConnectionId;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Repository interface for managing {@link Connection} entities.
+ *
+ * This repository provides basic CRUD operations for {@code Connection}
+ * entities identified by a composite primary key represented by {@link ConnectionId}.
+ *
+ * The composite key is based on the identifiers of the
+ * {@code user} and the {@code friend}.
+ */
 public interface ConnectionRepository extends CrudRepository<Connection, ConnectionId> {
 }
