3ba8d6804215a6e29ce239dd8332c3f1415e13a6
Caroline <bergere.caroline@outlook.fr>
2026-01-14 11:36:06 +0100

    feat: configure form login authentication with defaults

--- src/main/java/com/openclassrooms/paymybuddy/configuration/SpringSecurityConfig.java
+++ src/main/java/com/openclassrooms/paymybuddy/configuration/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.openclassrooms.paymybuddy.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,16 +20,9 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/login", "/register", "/css/**") // css/** pour permettre affichage
+        return http.authorizeHttpRequests(auth -> auth.requestMatchers("/register", "/css/**") // css/** pour permettre affichage
                         .permitAll().anyRequest().authenticated())
-
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/home", true)
-                        .permitAll())
-                .logout(logout -> logout
-                        .logoutSuccessUrl("/login?logout"));
-
-        return http.build();
+                .formLogin(Customizer.withDefaults()) // ✅ login automatique
+                .build();
     }
 }
