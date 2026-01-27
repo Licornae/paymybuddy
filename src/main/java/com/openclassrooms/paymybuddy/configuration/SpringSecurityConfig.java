78784044b7f4a2dcea0089c5d474a3f5f9ac2540
Caroline <bergere.caroline@outlook.fr>
2026-01-27 11:09:32 +0100

    feat: add login, register and add-connection pages with controllers

--- src/main/java/com/openclassrooms/paymybuddy/configuration/SpringSecurityConfig.java
+++ src/main/java/com/openclassrooms/paymybuddy/configuration/SpringSecurityConfig.java
@@ -2,7 +2,6 @@ package com.openclassrooms.paymybuddy.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,12 +23,14 @@ public class SpringSecurityConfig {
                 .csrf(csrf -> csrf.disable())
 
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/register", "/login", "/css/**").permitAll()
+                        .requestMatchers("/","/register", "/login", "/css/**").permitAll()
                         .anyRequest().authenticated())
 
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/transfer", true)
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/transfert", true)
+                        .failureUrl("/login?error")
                         .permitAll())
 
                 .logout(logout -> logout
